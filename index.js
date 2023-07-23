da24565b311a40efefeadf0cd9253941f5880e20
stephf2 <136709330+stephf2@users.noreply.github.com>
2023-07-23 19:59:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt-sync')({
-    history: require('prompt-sync-history')()
-  });
-const colours = require('ansi-colors');
-const rps = () => {
-    switch(Math.floor(Math.random()*3)){
+const prompt = require('prompt-sync')({sigint: true})
+const colours = require('ansi-colors')
+
+//random computer choice
+function computerRps() {
+    switch(Math.floor(Math.random()*3)) {
         case 0:
             return 'rock';
             break;
@@ -13,40 +13,67 @@ const rps = () => {
           case 2:
             return 'paper';
             break;
-            };
-      }
+    };
+}
 
-const userRps = (userInput) => {
+// player choice
+function userRps (userInput) {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput==='paper' || userInput ==='scissors'){
         return userInput;
     } 
   }
 
-while (userRps) {
-    const userInput = prompt('rock, paper, scissors: ')
-    prompt.history.save();  
-    if (userRps(userInput) === rps()) {
-        console.log(colours.yellow('Draw')) ;
-    } else if (userRps(userInput) === 'rock') {
-        if (rps() === 'paper') {
-        console.log(colours.red('Loss'));
+// win, loss or draw
+function whoWins(userRps, computerRps) {
+    let result
+    if (userRps === computerRps) {
+        result = colours.yellow('Draw')
+    } else if (userRps === 'rock') {
+        if (computerRps === 'paper') {
+            result = colours.red('You lose')
         } else {
-        console.log(colours.green('Win'));
+            result = colours.green('You win')
         }
-    } else if (userRps(userInput) === 'paper'){
-        if (rps() === 'scissors') {
-        console.log(colours.red('Loss'));
-        }else {
-        console.log(colours.green('Win'));
-        }
-    } else if (userRps(userInput) === 'scissors') {
-        if (rps() === 'rock') {
-        console.log(colours.red('Loss'));
+    } else if (userRps === 'paper') {
+        if (computerRps === 'scissors') {
+            result = colours.red('You lose')
         } else {
-        console.log(colours.green('Win'));
+            result = colours.green('You win')
+        }
+    } else if (userRps === 'scissors') {
+        if (computerRps === 'rock') {
+            result = colours.red('You lose')
+        } else {
+            result = colours.green('You win')
         }
     }
+    return result;
 }
 
+//score
+function score() {
+    let scores = {
+        wins: 0, 
+        loses: 0, 
+        draws: 0
+    };
 
+    if (whoWins === 'You win') {
+        scores.wins ++;
+    } else if (whoWins === 'You lose') {
+        scores.loses ++;
+    } else if (whoWins === 'Draw') {
+        scores.draws ++;
+    }
+    return scores;
+}
+
+while (userRps){
+const userInput = prompt('Rock, paper or scissors: ')
+console.log(whoWins(userInput, computerRps()));
+const records = prompt('See scores? y / n: ')
+if (records === 'y') {
+    console.log(score());
+}
+}
